b58e124b84b4a997f5be846c88ffba4606e2b36e
Ryan Brooks <brooksryan1234@gmail.com>
2024-12-09 22:14:07 -0500

    Started code

---
+++ src/base/data.py
@@ -0,0 +1,250 @@
+import os
+import torch
+from torch.utils.data import Dataset, DataLoader
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+from collections import Counter
+import random
+import numpy as np
+
+class LoopDataset(Dataset):
+    def __init__(self, loop_ids, cfg):        
+        self.EMB1 = [] # program embedding1 (reference)
+        self.EMB2 = [] # program embedding2 (transformed)
+        self.TR = [] # transformation
+        self.Y = [] # speedups (target)
+        self.loop_mapping = [] # mapping loop groups to their index in the dataset
+        self.embed_dim = 0
+        self.tr_dim = 0
+        start_idx = 0        
+        for l_id in loop_ids:
+            fpath = os.path.join(cfg['data_path'], l_id, l_id)
+            try:
+                # load the embeddings (n_transfs x n_layers x emb_dim)
+                embeds = getattr(self, f'load_{cfg["embedding_model"]}')(fpath)
+            except:
+                #print(f'Loop group {l_id} excluded: embedding not found')
+                continue
+            speedups = torch.load(f'{fpath}_speedup_refs.pt', weights_only=True, map_location='cpu')
+            transforms = torch.load(f'{fpath}_transformation_encodings.pt', weights_only=True, map_location='cpu')
+            n_transfs = speedups.shape[0]
+            ref_fname = [f for f in os.listdir(os.path.join(cfg['data_path'],l_id)) if f.endswith('c.0.c')][0]
+            ref_file_size = os.path.getsize(os.path.join(cfg['data_path'], l_id, ref_fname))
+            assert(embeds.shape[0] == n_transfs == transforms.shape[0])
+            if n_transfs < cfg['min_transformations'] or n_transfs > cfg['max_transformations']:
+                #print(f'Loop {l_id} excluded: less than {cfg['min_transformations']} transformations')
+                continue
+            elif ref_file_size > cfg['max_source_size']:
+                #print(f'Loop {l_id} excluded: file size over {cfg['max_source_size']} bytes')
+                continue
+            elif torch.max(speedups) > cfg['max_speedup']:
+                # print(f'Loop {l_id} excluded: contains speedup over {cfg['max_speedup']} ({speedups})')
+                continue
+            else:
+                if cfg['embedding_layer'] == 'last':
+                    if cfg['n_embeddings'][0]:
+                        self.EMB1.append(embeds[0,-1].repeat(n_transfs,1))
+                    if cfg['n_embeddings'][1]:
+                        self.EMB2.append(embeds[:,-1])
+                if cfg['n_embeddings'][2]:
+                    self.TR.append(transforms)
+                self.Y.append(speedups)
+                self.loop_mapping.append({'id':l_id, 'start':start_idx, 'end':start_idx+n_transfs})
+                start_idx += n_transfs
+        if cfg['n_embeddings'][0]:
+            self.EMB1 = torch.vstack(self.EMB1).float()
+            self.embed_dim = self.EMB1.shape[1]
+        if cfg['n_embeddings'][1]:
+            self.EMB2 = torch.vstack(self.EMB2).float()
+            self.embed_dim = self.EMB2.shape[1]
+        if cfg['n_embeddings'][2]:            
+            self.TR = torch.vstack(self.TR).float()
+            self.tr_dim += self.TR.shape[1]
+        print(self.embed_dim)
+        print(self.tr_dim)
+        self.Y = torch.hstack(self.Y).float()
+        if cfg['task'] == 'classification':
+            self.Y = torch.bucketize(self.Y, torch.tensor(cfg['class_splits']), right=True) - 1
+        print(f'{len(loop_ids)-len(self.loop_mapping)} loop groups removed for this split')
+        # if cfg['n_embeddings'] == 2:
+        #     assert self.EMB1.shape[0] == self.EMB2.shape[0] == self.TR.shape[0] == self.Y.shape[0]
+        #     print(f'EMB1: {self.EMB1.shape} EMB2: {self.EMB2.shape} TR: {self.TR.shape} Y: {self.Y.shape}')
+        # elif cfg['n_embeddings'] == 1:
+        #     assert self.EMB1.shape[0] == self.TR.shape[0] == self.Y.shape[0]
+        #     print(f'EMB1: {self.EMB1.shape} TR: {self.TR.shape} Y: {self.Y.shape}')
+
+    def load_llvm_llmcompiler(self, fpath):
+        #emb = torch.load(f'{fpath}_llvm_llmcompiler_7b_ftd_0.pt', weights_only=True, map_location='cpu')        
+        return torch.load(f'{fpath}_llvm_llm_compiler_13b_all.pt', weights_only=True, map_location='cpu') 
+    
+    def load_source_llmcompiler(self, fpath):
+        return torch.load(f'{fpath}_source_llmcompiler_13b_ftd_all.pt', weights_only=True, map_location='cpu')        
+
+    def load_source_codellama(self, fpath):
+        return torch.load(f'{fpath}_source_codellama_13b_hf_all.pt', weights_only=True, map_location='cpu')
+
+    def load_source_codet5p(self, fpath):
+        emb = torch.load(f'{fpath}_source_codet5p_110m_embedding_all.pt', weights_only=True, map_location='cpu')
+        return emb.unsqueeze(1)
+
+    def load_source_coderankembed(self, fpath):
+        emb = torch.load(f'{fpath}_source_code_rank_embed_all.pt', weights_only=True, map_location='cpu')        
+        return emb.unsqueeze(1)
+        
+    def __len__(self):
+        return self.Y.shape[0]
+    
+    def __getitem__(self, idx):
+        if self.EMB1 != []:
+            if self.EMB2 != []:
+                if self.TR != []:
+                    return self.EMB1[idx], self.EMB2[idx], self.TR[idx], self.Y[idx]
+                else:
+                    return self.EMB1[idx], self.EMB2[idx], self.Y[idx]
+            else:
+                if self.TR != []:
+                    return self.EMB1[idx], self.TR[idx], self.Y[idx]
+                else:
+                    return self.EMB1[idx], self.Y[idx]
+        else:
+            if self.EMB2 != []:
+                if self.TR != []:
+                    return self.EMB2[idx], self.TR[idx], self.Y[idx]
+                else:
+                    return self.EMB2[idx], self.Y[idx]
+            else:
+                if self.TR != []:
+                    return self.TR[idx], self.Y[idx]
+                else:
+                    return self.Y[idx]
+        
+        # all_lists = [self.EMB1, self.EMB2, self.TR]
+        # enabled_lists = [ls[idx] for ls in all_lists if ls != []]
+        # print(enabled_lists)
+        # return tuple(enabled_lists)
+
+        # if self.TR != []:
+        #     if self.EMB2 != []:
+        #         return self.EMB1[idx], self.EMB2[idx], self.TR[idx], self.Y[idx]
+        #     else:
+        #         return self.EMB1[idx], self.TR[idx], self.Y[idx]
+        # else:
+        #     if self.EMB2 != []:
+        #         return self.EMB1[idx], self.EMB2[idx], self.Y[idx]
+        #     else:
+        #         return self.EMB1[idx], self.Y[idx]
+
+# returns three dataloaders for training, validation and testing
+def get_data_loaders(cfg):
+    # grab all available loop groups
+    loop_ids = [d for d in os.listdir(cfg['data_path']) if len(d) == 36]
+    # perform train/val/test split
+    if cfg['stratification'] == 'random':
+        tr_ids, va = train_test_split(loop_ids, test_size=0.2)
+        va_ids, te_ids = train_test_split(va, test_size=0.5)
+    else:
+        if cfg['stratification'] == 'binary':
+            loop_labels = balanced_loop_groups_binary(loop_ids, cfg['data_path'])
+        elif cfg['stratification'] == 'clustered':
+            loop_labels = balanced_loop_groups_clustered(loop_ids, cfg['data_path'])
+        print(f'Loop groups stratified into {Counter(loop_labels)}')
+        tr_ids, va, _, lab_va = train_test_split(loop_ids, loop_labels, test_size=0.2, stratify=loop_labels)
+        va_ids, te_ids = train_test_split(va, test_size=0.5, stratify=lab_va)
+    print(f'Total loop groups: {len(loop_ids)} Train: {len(tr_ids)} Val: {len(va_ids)} Test: {len(te_ids)}')
+    tr_d = LoopDataset(tr_ids, cfg)
+    va_d = LoopDataset(va_ids, cfg)
+    te_d = LoopDataset(te_ids, cfg)
+    print(f'Total datapoints: {len(tr_d)+len(va_d)+len(te_d)} Train: {len(tr_d)} Valid: {len(va_d)} Test: {len(te_d)}')
+    return (
+        DataLoader(tr_d, batch_size=cfg['batch_size'], shuffle=True, num_workers=cfg['n_workers'], pin_memory=True),
+        DataLoader(va_d, batch_size=len(va_d), shuffle=False, num_workers=cfg['n_workers'], pin_memory=True),
+        DataLoader(te_d, batch_size=len(te_d), shuffle=False, num_workers=cfg['n_workers'], pin_memory=True)
+    )
+
+def seed_worker(worker_id):
+    worker_seed = torch.initial_seed() % 2**32
+    np.random.seed(worker_seed)
+    random.seed(worker_seed)
+    torch.manual_seed(worker_seed)
+    torch.cuda.manual_seed(worker_seed)
+
+def set_seed(seed):
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+# returns three dataloaders for training, validation and testing
+def get_data_loaders_wandb(cfg):
+    torch.manual_seed(cfg['seed'])
+    np.random.seed(cfg['seed'])
+    random.seed(cfg['seed'])
+    # grab all available loop groups
+    loop_ids = [d for d in os.listdir(cfg['data_path']) if len(d) == 36]
+    # perform train/val/test split
+    if cfg['stratification'] == 'random':
+        tr_ids, va_ids = train_test_split(loop_ids, test_size=0.2, random_state=cfg['seed'])
+    else:
+        if cfg['stratification'] == 'binary':
+            loop_labels = balanced_loop_groups_binary(loop_ids, cfg['data_path'])
+        elif cfg['stratification'] == 'clustered':
+            loop_labels = balanced_loop_groups_clustered(loop_ids, cfg['data_path'])
+        elif cfg['stratification'] == 'majority':
+            loop_labels = balanced_loop_groups_majority(loop_ids, cfg['data_path'], cfg['class_splits'], cfg['classes'])
+        print(f'Loop groups stratified into {Counter(loop_labels)}')
+        tr_ids, va_ids, _, lab_va = train_test_split(loop_ids, loop_labels, test_size=0.2, stratify=loop_labels)
+    print(f'Total loop groups: {len(loop_ids)} Train: {len(tr_ids)} Val: {len(va_ids)}')
+    tr_d = LoopDataset(tr_ids, cfg)
+    va_d = LoopDataset(va_ids, cfg)
+    print(f'Total datapoints: {len(tr_d)+len(va_d)} Train: {len(tr_d)} Valid: {len(va_d)}')
+    # sampler = torch.utils.data.RandomSampler(tr_d, generator=torch.Generator().manual_seed(cfg['seed']))
+    # set_seed(cfg['seed'])
+    return (
+        DataLoader(tr_d, batch_size=cfg['batch_size'], shuffle=True, num_workers=cfg['n_workers'], pin_memory=True, worker_init_fn=seed_worker),
+        DataLoader(va_d, batch_size=len(va_d), shuffle=False, num_workers=cfg['n_workers'], pin_memory=True)
+    )
+
+# returns T/F labels for each loop group based on 
+# speedup values, true if loop group contains at least 
+# one transformation with speedup >= 1, false otherwise
+def balanced_loop_groups_binary(loop_ids, data_path):
+    loop_id_labels = []
+    for l_id in loop_ids:
+        fname = os.path.join(data_path, l_id, f'{l_id}_speedup_refs.pt')
+        speedups = torch.load(fname, weights_only=True, map_location='cpu')
+        good_speedups = torch.where(speedups > 1, 1, 0).sum()
+        loop_id_labels.append('good' if good_speedups > 0 else 'bad')
+    return loop_id_labels
+
+# returns cluster labels for each loop group based on
+# the speedup distribution statistics
+def balanced_loop_groups_clustered(loop_ids, data_path):
+    loop_id_labels = []
+    all_stats = []
+    for l_id in loop_ids:
+        fname = os.path.join(data_path, l_id, f'{l_id}_speedup_refs.pt')
+        speedups = torch.load(fname, weights_only=True, map_location='cpu')
+        stats = torch.tensor([speedups.max(), speedups.min(), speedups.quantile(0.25), speedups.quantile(0.75), torch.exp(torch.log(speedups).mean())])
+        all_stats.append(stats)
+    all_stats = torch.vstack(all_stats)
+    pipe = Pipeline([
+        ('scaler', StandardScaler()),
+        ('kmeans', KMeans(n_clusters=4, n_init=50, init='k-means++'))
+    ])
+    loop_id_labels = [f'group_{l}' for l in pipe.fit_predict(all_stats)]
+    return loop_id_labels
+
+def balanced_loop_groups_majority(loop_ids, data_path, splits, classes):
+    loop_id_labels = []
+    for l_id in loop_ids:
+        fname = os.path.join(data_path, l_id, f'{l_id}_speedup_refs.pt')
+        speedups = torch.load(fname, weights_only=True, map_location='cpu')
+        loop_id_stats = []
+        for i in range(len(splits)-1):
+            loop_id_stats.append(torch.where((speedups > splits[i]) & (speedups < splits[i+1]), 1, 0).sum())
+        loop_id_labels.append(classes[loop_id_stats.index(max(loop_id_stats))])
+    return loop_id_labels
---
+++ src/base/train.py
@@ -0,0 +1,129 @@
+import torch
+import torch.nn.functional as F
+from torcheval.metrics.functional import multiclass_accuracy
+import matplotlib.pyplot as plt
+from sklearn.metrics import ConfusionMatrixDisplay
+
+class Trainer:
+    def __init__(self, device, model, cfg):
+        self.device = device
+        self.model = model
+        self.max_epochs = cfg['max_epochs']
+        self.task = cfg['task']
+        self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=cfg['learning_rate'], weight_decay=cfg['weight_decay'])
+        #self.scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(self.optimizer, T_max=cfg['max_epochs'])
+    
+    def compute_loss(self, pred, tgt):
+        if self.task == 'regression':
+            return F.mse_loss(pred, tgt, reduction='mean')
+        elif self.task == 'classification':
+            return F.cross_entropy(pred, tgt, reduction='mean')
+
+    def train_epoch(self, loader):
+        self.model.train()
+        tot_loss = 0
+        for batch in loader:
+            ## send all tensors to device (x1, x2, ..., tgt)
+            b = [t.to(self.device) for t in batch]
+            self.optimizer.zero_grad()
+            pred = self.model(b[:-1]) # x1, x2, ...
+            loss = self.compute_loss(pred, b[-1])
+            loss.backward()
+            self.optimizer.step()
+            tot_loss += (loss.item() * b[0].size(0))
+        return tot_loss / len(loader.dataset)
+
+    def evaluate(self, loader):
+        self.model.eval()
+        tot_loss = 0
+        with torch.no_grad():
+            for batch in loader:
+                b = [t.to(self.device) for t in batch]
+                pred = self.model(b[:-1])
+                loss = self.compute_loss(pred, b[-1])
+                tot_loss += (loss.item() * b[0].size(0))
+        return tot_loss / len(loader.dataset)
+    
+    def predict(self, loader):
+        self.model.eval()
+        preds, tgts = [], []
+        with torch.no_grad():
+            for batch in loader:
+                b = [t.to(self.device) for t in batch]
+                preds.append(self.model(b[:-1]))
+                tgts.append(b[-1])
+        if self.task == 'regression':
+            return torch.hstack(preds), torch.hstack(tgts)
+        elif self.task == 'classification':
+            return torch.vstack(preds), torch.hstack(tgts)
+
+    def train(self, tr_loader, va_loader):
+        tr_losses = []
+        va_losses = []
+
+        best_va_loss = float('inf')
+        no_improvement_epochs = 0
+        patience = 5
+
+        for epoch in range(self.max_epochs):
+            tr_loss = self.train_epoch(tr_loader)
+            va_loss = self.evaluate(va_loader)
+            #self.scheduler.step()
+            tr_losses.append(tr_loss)
+            va_losses.append(va_loss)
+            print(f'Epoch: {epoch+1}\tTr Loss: {tr_loss:.3f}\tVal Loss: {va_loss:.3f}')
+
+            if va_loss < best_va_loss:
+                best_va_loss = va_loss
+                no_improvement_epochs = 0
+                torch.save(self.model.state_dict(), 'best_model.pth')
+            else:
+                no_improvement_epochs += 1  # Increment counter for no improvement
+
+            if no_improvement_epochs >= patience:
+                print(f'Early stopping triggered after {epoch+1} epochs.')
+                break
+
+        return tr_losses, va_losses
+
+def eval_metrics(preds, tgts, figures=False, mapping=None, type='regression', classes=None):
+    if type == 'regression':
+        eval_metrics_regression(preds, tgts, figures, mapping)
+    elif type == 'classification':
+        eval_metrics_classification(preds, tgts, figures, mapping, classes)
+
+def eval_metrics_classification(preds, tgts, figures=False, mapping=None, classes=None):
+    preds, tgts = preds.cpu(), tgts.cpu()
+    acc1 = multiclass_accuracy(preds, tgts)
+    print(f'Accuracy ({len(classes)}-way): {acc1:.4f}')
+    acc2 = torch.eq(preds.argmax(dim=1)>2, tgts>2).float().mean() # NEED more elegant code here!!!
+    print(f'Accuracy (speedup/slowdown): {acc2:.4f}')
+    ConfusionMatrixDisplay.from_predictions(tgts, torch.argmax(preds,dim=1), display_labels=classes, normalize='true')
+    plt.show()
+
+def eval_metrics_regression(preds, tgts, figures=False, mapping=None):
+    # calculate the accuracy of the model on each datapoint
+    preds, tgts = preds.cpu(), tgts.cpu()
+    acc = torch.eq(preds>1, tgts>1).float().mean()
+    print(f'Accuracy (speedup/slowdown): {acc:.4f}')
+    # loop level analysis (requires loop groups to have at least 3 transformations)
+    loop_acc = torch.zeros(3)
+    if mapping != None:
+        for loop in mapping:
+            loop_preds = preds[loop['start']:loop['end']]
+            loop_tgts = tgts[loop['start']:loop['end']]            
+            _, idx_tgt = torch.topk(loop_tgts, 1)
+            for i, k in enumerate([1,2,3]):
+                _, idx_pred = torch.topk(loop_preds, k)
+                loop_acc[i] += (1 if len(set(idx_tgt.numpy()).intersection(set(idx_pred.numpy()))) > 0 else 0)
+        print(f'acc top-1: {loop_acc[0]/len(mapping):.4f} top-2: {loop_acc[1]/len(mapping):.4f} top-3: {loop_acc[2]/len(mapping):.4f}')
+    # plot the predicted vs true speedups
+    if figures:
+        plt.figure()
+        plt.scatter(tgts, preds, marker='.', color='gray', alpha=0.5)
+        plt.xlabel('True Speedup')
+        plt.ylabel('Predicted Speedup')
+        plt.plot([0,preds.max()],[0,preds.max()], color='lightgreen', linestyle='dashed')
+        plt.plot([1,1],[0,preds.max()], color='orange', linestyle='dashed')
+        plt.plot([0,tgts.max()],[1,1], color='orange', linestyle='dashed')
+        plt.show()
